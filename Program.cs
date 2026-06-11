116ec7c64d86f44a787794a5bf200fbe9732f4d4
Jean Ortiz <jortiz@oceloteminerals.com>
2026-06-11 09:38:38 -0500

    Implement Identity with PostgreSQL support and add initial migration files

--- Program.cs
+++ Program.cs
@@ -1,16 +1,32 @@
 using Application;
+using Infrastructure.Adapters.Out.Persistence;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplication();
+
+builder.Services.AddDbContext<AppDbContext>(opts =>
+    opts.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services
+    .AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
 
 builder.Services.AddRazorPages()
     .WithRazorPagesRoot("/src/infrastructure/adapters/in/razor");
 
-
 var app = builder.Build();
+
+app.MapIdentityApi<IdentityUser>();   // expone /register, /login, /refresh, etc.
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 app.MapRazorPages();
